9f4efcc0d0f9809e3735084b76cacc190cf75b85
brhane.t <bretama99@gmail.com>
2023-10-19 06:52:19 +0300

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+        <meta http-equiv="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>Laravel</title>
+    </head>
+    <body class="antialiased">
+        <a href="">Admin Dashboard</a>
+        
+    </body>
+</html>
